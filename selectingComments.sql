1a6a0941ea31756b30ef99b0e46682356ea966df
Shivam Tayal <shivamtayal@icloud.com>
2021-08-29 17:11:29 +0530

    Update selectingComments.sql

--- selectingComments.sql
+++ selectingComments.sql
@@ -6,8 +6,7 @@ JOIN ParentChildComment p ON c.comment_id = p.childComment_id
 WHERE 
    p.parentComment_id = 5
 ORDER BY 
-   c.upvote * c.downvote DESC,
-   c.created_date DESC;
+   POWER((c.upvote - c.downvote) * c.created_date, 2) DESC;
 
 --The query to retrieve all the parent comments, eg for comment 5
 SELECT c.*
@@ -17,9 +16,7 @@ JOIN ParentChildComment p ON c.comment_id = p.childComment_id
 WHERE 
    p.childComment_id = 5
 ORDER BY 
-   c.upvote * c.downvote DESC,
-   c.created_date DESC;
-
+   POWER((c.upvote - c.downvote) * c.created_date, 2) DESC;
 --Note the code will work for all comment id we just need to put it inside a loop
 --where value of p.childComment_id keep on inc (as seen in line 18)
 --we run the loop till all comments are retrieved.
